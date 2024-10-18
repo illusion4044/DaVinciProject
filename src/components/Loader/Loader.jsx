6934d72d787b63d023755f286f26aeb8c12b711d
nasteap2015 <149483716+nasteap2015@users.noreply.github.com>
2024-10-18 20:15:42 +0200

    Spinner library added/Loader component done

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,14 @@
+import { InfinitySpin } from 'react-loader-spinner';
+
+const Loader = () => {
+    return (
+        <InfinitySpin
+            visible={true}
+            width="200"
+            color="#407BFF57"
+            ariaLabel="infinity-spin-loading"
+        />
+    );
+};
+
+export default Loader;
