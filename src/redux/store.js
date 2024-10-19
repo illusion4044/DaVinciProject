1704dcaecb977bcc4402fd6a6525e47575ed9104
OksanaGukova <kseniagukova@ukr.net>
2024-10-19 20:38:50 +0300

    add data to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,7 @@ import storage from 'redux-persist/lib/storage';
 import authSlice from './auth/slice';
 import usersSlice from './users/slice';
 import filtersSlice from './filters/slice';
+import waterReducer from './water/slice.js';
 import {
   FLUSH,
   REHYDRATE,
@@ -19,21 +20,28 @@ const authPersistConfig = {
     whitelist: ['token'],
 };
 
+const waterPersistConfig = {
+  key: 'water',
+  storage,
+  whitelist: ['dailyNorma'],
+};
+
 const persistedAuthReducer = persistReducer(authPersistConfig, authSlice);
+const persistedWaterReducer = persistReducer(waterPersistConfig, waterReducer);
 
 export const store = configureStore({
-    reducer: {
-        auth: persistedAuthReducer,
-        users: usersSlice,
-        filters: filtersSlice,
-    },
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: {
-      
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }),
+  reducer: {
+    auth: persistedAuthReducer,
+    users: usersSlice,
+    filters: filtersSlice,
+    water: persistedWaterReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
