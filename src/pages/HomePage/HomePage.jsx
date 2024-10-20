ad1c255e7b13778eb4cdc390d19026137e26eaf4
Halenina <galenina20032101@gmail.com>
2024-10-21 00:35:02 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
-import DailyNorma from '../components/Stats/DailyNorma.js';
-import WaterRatioPanel from '../components/Trackers/WaterRatioPanel.js';
-import TodayWaterList from '../components/Stats/TodayWaterList.js';
-import MonthStatsTable from '../components/Stats/MonthStatsTable.js';
+import DailyNorma from '../../components/DailyNorma/DailyNorma';
+import WaterRatioPanel from '../../components/WaterRatioPanel/WaterRatioPanel';
+import TodayWaterList from '../../components/TodayWaterList/TodayWaterList';
+import MonthStatsTable from '../../components/MonthStatsTable/MonthStatsTable';
 
 export default function HomePage() {
   return (
