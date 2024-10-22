9bf189a74fe5f9d3fee3791b24863c1caa0c3f38
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-10-22 12:06:15 +0300

    add code

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -10,11 +10,7 @@ export default function DailyNorma() {
     <div>
       <h4>My daily Norma</h4>
       <p>DailyNorma: {liters} L</p>
-<<<<<<< Updated upstream
-      <button onClick={() => {}}>Edit</button>
-=======
       <button onClick={handleClick}>Edit</button>
->>>>>>> Stashed changes
     </div>
   );
 }
