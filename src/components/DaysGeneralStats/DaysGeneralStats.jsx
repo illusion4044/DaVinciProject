0f12125eeec5e5a7385349afad98492e14298f6b
nasteap2015 <149483716+nasteap2015@users.noreply.github.com>
2024-10-23 12:20:02 +0200

    DaysGeneralStats with styles

--- src/components/DaysGeneralStats/DaysGeneralStats.jsx
+++ src/components/DaysGeneralStats/DaysGeneralStats.jsx
@@ -1,26 +1,36 @@
 import css from './DaysGeneralStats.module.css';
 import moment from 'moment';
 
-export const DaysGeneralStats = ({
+const DaysGeneralStats = ({
   chosenDay,
   dailyNorma,
   normFulfillment,
   servings
 }) => {
 
-  function convertFormat(date) {
-        const date = moment(date, 'DD.MM.YYYY');
+  function convertFormat(fullDate) {
+        const date = moment(fullDate, 'DD.MM.YYYY');
         return date.format('D, MMMM');
   }
 
   const convertedDate = convertFormat(chosenDay);
 
   return (
-     <div className={css.statsContainer}>
+     <div className={css.dailyStatsContainer}>
       <h3 className={css.containerTitle}>{convertedDate}</h3>
-      <p className={css.statsField}>Daily norma: <span className={css.statsValue}>{dailyNorma}</span></p>
-      <p className={css.statsField}>Fulfillment of the daily norm: <span className={css.statsValue}>{normFulfillment}</span></p>
-      <p className={css.statsField}>How many servings of water: <span className={css.statsValue}>{servings}</span></p>
+      <span className={css.statContainer}>
+        <p className={css.statsField}>Daily norma:</p>
+        <p className={css.statsValue}>{dailyNorma}</p>
+      </span>
+      <span className={css.statContainer}>
+        <p className={css.statsField}>Fulfillment of the daily norm:</p>
+        <p className={css.statsValue}>{normFulfillment}</p>
+      </span>
+      <span className={css.statContainer}>
+        <p className={css.statsField}>How many servings of water:</p>
+        <p className={css.statsValue}>{servings}</p>
+      </span>
     </div>
   )
 }
+export default DaysGeneralStats;
