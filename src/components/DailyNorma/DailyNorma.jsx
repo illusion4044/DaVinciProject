347910e83d190f448e069c0aca399dbf107dbd20
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-10-24 18:37:33 +0300

    add code button Edit ok

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -1,18 +1,29 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { openDailyModal } from '../../redux/water/slice.js';
 import DailyNormaModal from '../DailyNormaModal/DailyNormaModal.jsx';
+import { useState } from 'react';
 
 export default function DailyNorma() {
   const mlLiters = useSelector(state => state.water.dailyPortions);
   const liters = mlLiters / 1000;
   const dispatch = useDispatch();
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openDailyModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeDailyModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
       <h4>My daily Norma</h4>
       <p>DailyNorma: {liters} L</p>
       <button onClick={() => dispatch(openDailyModal())}>Edit</button>
-      {openDailyModal && <DailyNormaModal />}
+      {isModalOpen && <DailyNormaModal closeModal={closeDailyModal} />}
     </div>
   );
 }
