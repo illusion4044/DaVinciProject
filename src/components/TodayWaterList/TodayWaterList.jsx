c743522544b45c7177198bbe1b609ce3456fb96b
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-10-25 11:06:43 +0300

    changed names for files

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -1,46 +1,33 @@
 // import WaterRatioPanel from '../WaterRatioPanel/WaterRatioPanel.jsx';
 // import TodayListModal from '../TodayListModal/TodayListModal.jsx';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import css from './TodayWaterList.module.css';
 
 // import { selectWater } from "../../redux/waterPortion/selectors.js";
 // import waterPortion from "../UserLogOutModal/UserLogoutModal.jsx";
 // import css from "./TodayWaterList.module.css";
 
 export default function TodayWaterList() {
-  const portionWater = useSelector(state => state.water.item);
+  const mlLiters = useSelector(state => state.water.dailyPortions);
+  const dispatch = useDispatch();
 
   //   const time = useSelector(state => state.time.item);
   //const contacts = useSelector(selectTime);
   return (
     <>
       <h3>Today</h3>
-      <ul>
-        <li>
-          <svg>
-            <use href="src/img/icons.svg#icon-Group-4"></use>
-          </svg>
-          {/* TodayWaterList:{portionWater}
-          TodayWaterList: {} */}
-          <button>
-            <svg>
-              <use href="src/img/icons.svg#icon-pencil-square"></use>
-            </svg>
-          </button>
-          <button>
-            <svg>
-              <use href="src/img/icons.svg#icon-trash"></use>
-            </svg>
-          </button>
-        </li>
+      <ul className={css.wtList}>
+        {mlLiters.map(portion => (
+          <li className={css.list} key={portion.id}></li>
+        ))}
       </ul>
-      {/* <ul className={css.wtList}>
-//       {rightFilter.map((portion) => ( */}
-      {/* //         <li className={css.list} key={portion.id}>
-//           <Portion data={portion} /> */}
-      {/* //         </li> */}
-      {/* //       ))} */}
-      {/* //     </ul> */}
-      <button onClick={() => {}}>+ Add Water</button>
+      {/* <button onClick={() => {}}> + Add Water</button> */}
     </>
   );
 }
+/* <use href="src/img/icons.svg#icon-Group-4"></use>;
+<use href="src/img/icons.svg#icon-pencil-square"></use>;
+<use href="src/img/icons.svg#icon-trash"></use>; */
+
+/* TodayWaterList:{portionWater}
+          TodayWaterList: {} */
