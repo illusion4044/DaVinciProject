aa7dad5314af83e48ecd1c09fb01913d6287cf61
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-10-26 14:41:09 +0300

    add code

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -1,54 +1,35 @@
-// import WaterRatioPanel from '../WaterRatioPanel/WaterRatioPanel.jsx';
-// import TodayListModal from '../TodayListModal/TodayListModal.jsx';
-
-import { useSelector, useDispatch } from 'react-redux';
+import WaterRatioPanel from '../WaterRatioPanel/WaterRatioPanel.jsx';
+import onSaveClick from '../TodayListModal/TodayListModal.jsx';
+import { useSelector } from 'react-redux';
 import css from './TodayWaterList.module.css';
-
-// import { selectWater } from "../../redux/waterPortion/selectors.js";
-
-// import waterPortion from "../UserLogOutModal/UserLogoutModal.jsx";
 
 export default function TodayWaterList() {
   const mlLiters = useSelector(state => state.water.dailyPortions);
 
-  const dispatch = useDispatch();
-
   return (
     <>
       <div className={css.container}>
-        <h4 className={css.name}>Today</h4>
-        <div className={css.containerList}>
-          <svg className={css.iconGlass}>
-            <use href="src/img/icons.svg#icon-Group-4"></use>
-          </svg>
+        <p className={css.name}>Today</p>
+        {onSaveClick && (
+          <div className={css.containerList}>
+            <svg className={css.iconGlass}>
+              <use href="src/img/icons.svg#icon-Group-4"></use>
+            </svg>
 
-          <ul className={css.wtList}>
-            {mlLiters.map(portion => (
-              <li className={css.list} key={portion.id}></li>
-            ))}
-          </ul>
-          <svg className={css.iconPencil}>
-            <use href="/public/icons.svg#icon-Vector"></use>
-          </svg>
-          <svg className={css.iconTrash}>
-            <use href="src/img/icons.svg#icon-Vector"></use>
-          </svg>
-        </div>
+            <ul className={css.wtList}>
+              {mlLiters.map(portion => (
+                <li className={css.list} key={portion.id}></li>
+              ))}
+            </ul>
+            <svg className={css.iconPencil}>
+              <use href="/public/icons.svg#icon-Vector"></use>
+            </svg>
+            <svg className={css.iconTrash}>
+              <use href="src/img/icons.svg#icon-Vector"></use>
+            </svg>
+          </div>
+        )}
       </div>
-
-      <h3>Today</h3>
-      <ul className={css.wtList}>
-        {mlLiters.map(portion => (
-          <li className={css.list} key={portion.id}></li>
-        ))}
-      </ul>
-      {/* <button onClick={() => {}}> + Add Water</button> */}
     </>
   );
 }
-/* <use href="src/img/icons.svg#icon-Group-4"></use>;
-<use href="src/img/icons.svg#icon-pencil-square"></use>;
-<use href="src/img/icons.svg#icon-trash"></use>; */
-
-/* TodayWaterList:{portionWater}
-          TodayWaterList: {} */
