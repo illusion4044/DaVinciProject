0f65da70f0503966a58a9e235370332a6121d837
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-10-26 09:19:14 +0300

    resolved conflicts

--- src/components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -1,10 +1,8 @@
-
 import ReactSlider from 'react-slider';
 import { useSelector } from 'react-redux';
 import { selectPercentPerDay } from '../../redux/water/selectors';
 import css from './WaterRatioPanel.module.css';
-import icon from "../../img/icons.svg";
-
+import icon from '../../img/icons.svg';
 
 export default function WaterRatioPanel() {
   // const rect1 = Number(useSelector(selectPercentPerDay));
@@ -12,7 +10,6 @@ export default function WaterRatioPanel() {
   const rect2 = 100 - rect1;
 
   return (
-
     <>
       <ReactSlider
         className="horizontal-slider"
@@ -20,50 +17,58 @@ export default function WaterRatioPanel() {
         trackClassName="example-track"
         renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
       />
-      {/* <div>
-        <button onClick={() => {}}> + Add Water</button>
-      </div> */}
-    </>
 
-    <div className={css.container}>
-      <div className={css.todayBarBox}>
-        <p className={css.today}>Today</p>
-        <div className={css.rectBox}>
-          <span
-            className={css.rectangle}
-            style={{
-              width: `${rect1}%`,
-              background: '#9EBBFF',
-            }}
-          ></span>
-          <span className={css.ellipse}></span>
-          <span
-            className={css.rectangle}
-            style={{
-              width: `${rect2}%`,
-              background: '#D7E3FF',
-            }}
-          ></span>
-          <span className={css.line} style={{left: 0, transform: 'translateY(310%)'}}></span>
-          <span className={`${css.line} ${css.line50}`}></span>
-          <span className={css.line} style={{ right: 0, transform: 'translateY(310%)' }}></span>
-          <span className={css.percent} style={{left: 0, transform: 'translate(-40%, 230%)'}}>0%</span>
-          <span className={`${css.percent} ${css.percentValue}`}>50%</span>
-          <span className={css.percent} style={{ right: 0, transform: 'translate(40%, 230%)' }}>100%</span>
+      <div className={css.container}>
+        <div className={css.todayBarBox}>
+          <p className={css.today}>Today</p>
+          <div className={css.rectBox}>
+            <span
+              className={css.rectangle}
+              style={{
+                width: `${rect1}%`,
+                background: '#9EBBFF',
+              }}
+            ></span>
+            <span className={css.ellipse}></span>
+            <span
+              className={css.rectangle}
+              style={{
+                width: `${rect2}%`,
+                background: '#D7E3FF',
+              }}
+            ></span>
+            <span
+              className={css.line}
+              style={{ left: 0, transform: 'translateY(310%)' }}
+            ></span>
+            <span className={`${css.line} ${css.line50}`}></span>
+            <span
+              className={css.line}
+              style={{ right: 0, transform: 'translateY(310%)' }}
+            ></span>
+            <span
+              className={css.percent}
+              style={{ left: 0, transform: 'translate(-40%, 230%)' }}
+            >
+              0%
+            </span>
+            <span className={`${css.percent} ${css.percentValue}`}>50%</span>
+            <span
+              className={css.percent}
+              style={{ right: 0, transform: 'translate(40%, 230%)' }}
+            >
+              100%
+            </span>
+          </div>
         </div>
+
+        <button className={css.addWaterBtn} type="button">
+          <svg className={css.icon}>
+            <use href={`${icon}#icon-plus-circle`} />
+          </svg>
+          Add Water
+        </button>
       </div>
-     
-      <button className={css.addWaterBtn} type="button" >
-        <svg className={css.icon}>
-          <use href={`${icon}#icon-plus-circle`} />
-        </svg>
-        Add Water
-      </button>
-    </div>
-    
-          
-
-
+    </>
   );
 }
-
