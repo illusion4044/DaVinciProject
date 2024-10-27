fef8e8ea37da99975c5debe1c95d9b3f111560b0
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-10-27 19:33:48 +0200

    fix code

--- src/redux/water/selectors.js
+++ src/redux/water/selectors.js
@@ -2,4 +2,3 @@ export const selectGender = state => state.water.gender;
 export const selectDailyNorma = state => state.water.dailyNorma;
 export const selectSelectedItem = state => state.water.selectedItem;
 export const selectPercentPerDay = state => state.water.percentPerDay;
-export const selectDailyNormaPortions = state => state.water.dailyPortions;
