b88a707fad2912a1943318b6c7fab4b1b273750b
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-10-27 11:09:28 +0200

    add code

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -1,32 +1,50 @@
-import WaterRatioPanel from '../WaterRatioPanel/WaterRatioPanel.jsx';
 import onSaveClick from '../TodayListModal/TodayListModal.jsx';
-import { useSelector } from 'react-redux';
+import selectedItem from '../TodayListModal/TodayListModal.jsx';
+import { useSelector, useDispatch } from 'react-redux';
 import css from './TodayWaterList.module.css';
+import { openTodayModal, closeTodayModal } from '../../redux/water/slice.js';
+import TodayListModal from '../TodayListModal/TodayListModal.jsx';
 
 export default function TodayWaterList() {
-  const mlLiters = useSelector(state => state.water.dailyPortions);
+  const dispatch = useDispatch();
+  const isModalOpen = useSelector(state => state.water.isTodayModalOpen);
 
   return (
     <>
       <div className={css.container}>
         <p className={css.name}>Today</p>
+        {!onSaveClick && (
+          <>
+            <div className={css.containerList}>
+              <svg className={css.iconGlass}>
+                <use href="src/img/icons.svg#icon-Group-4"></use>
+              </svg>
+              <p className={css.amount}>
+                {selectedItem ? selectedItem.amount : '0'} ml
+              </p>
+              <p className={css.time}>
+                {selectedItem ? selectedItem.time : '00:00'}
+              </p>
+              <svg className={css.iconPencil}>
+                <use href="/public/icons.svg#icon-Vector"></use>
+              </svg>
+              <svg className={css.iconTrash}>
+                <use href="src/img/icons.svg#icon-Vector"></use>
+              </svg>
+            </div>
+          </>
+        )}
         {onSaveClick && (
-          <div className={css.containerList}>
-            <svg className={css.iconGlass}>
-              <use href="src/img/icons.svg#icon-Group-4"></use>
-            </svg>
-
-            <ul className={css.wtList}>
-              {mlLiters.map(portion => (
-                <li className={css.list} key={portion.id}></li>
-              ))}
-            </ul>
-            <svg className={css.iconPencil}>
-              <use href="/public/icons.svg#icon-Vector"></use>
-            </svg>
-            <svg className={css.iconTrash}>
-              <use href="src/img/icons.svg#icon-Vector"></use>
-            </svg>
+          <div className={css.containerBtn}>
+            <p className={css.btn} onClick={() => dispatch(openTodayModal())}>
+              <svg className={css.iconBtn}>
+                <use href="src/img/icons.svg#icon-outline"></use>
+              </svg>
+              Add Water
+            </p>
+            {isModalOpen && (
+              <TodayListModal onClose={() => dispatch(closeTodayModal())} />
+            )}
           </div>
         )}
       </div>
