99102bd3a5d58de3aff4a62b0b408c6dce0b3468
GalinaKucherenko <galina.mail.k.n@gmail.com>
2024-10-29 23:47:00 +0200

    add css for HomePage, TodayWaterList

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -32,16 +32,19 @@ export default function TodayWaterList() {
                   <use href="src/img/icons.svg#icon-Group-4"></use>
                 </svg>
                 <li className={css.amount}>{amount} ml</li>
-                <li className={css.amount}>{time} PM</li>
+                <li className={css.time}>{time} PM</li>
 
-                <svg className={css.iconPencil}>
-                  <use href="/public/icons.svg#icon-Vector"></use>
-                </svg>
-                <svg className={css.iconTrash}>
-                  <use href="src/img/icons.svg#icon-Vector"></use>
-                </svg>
+                <div className={css.icons}>
+                  <svg className={css.iconPencil}>
+                    <use href="/public/icons.svg#icon-Vector"></use>
+                  </svg>
+                  <svg className={css.iconTrash}>
+                    <use href="src/img/icons.svg#icon-Vector"></use>
+                  </svg>
+                </div>
               </ul>
             </div>
+            <div className={css.line}></div>
           </>
         )}
 
