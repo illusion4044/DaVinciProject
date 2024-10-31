d0914a0d927a86d8a5721bbad350015224fd0b06
Andrii <117983189+illusion4044@users.noreply.github.com>
2024-11-01 00:04:13 +0200

    month stats refresh fix

--- src/components/MonthStatsTable/MonthStatsTable.jsx
+++ src/components/MonthStatsTable/MonthStatsTable.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useMemo, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchMonthlyPortionsThunk } from '../../redux/water/operations';
+import { selectDailyNorma } from '../../redux/water/selectors';
 import styles from './MonthStatsTable.module.css';
 import DaysGeneralStats from '../DaysGeneralStats/DaysGeneralStats';
-
-import sprite from "../../img/icons.svg"
-console.log(sprite)
-
+import sprite from "../../img/icons.svg";
 
 const MonthStatsTable = () => {
   const currentDate = new Date();
@@ -14,71 +12,47 @@ const MonthStatsTable = () => {
   const [selectedMonth, setSelectedMonth] = useState(currentDate.getMonth());
   const [selectedDay, setSelectedDay] = useState(null);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-
   const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December',
   ];
-
   const dispatch = useDispatch();
-  const monthlyPortions = useSelector((state) => state.water.monthlyPortions); 
+  const dailyNorm = useSelector(selectDailyNorma); // Отримуємо норму води
+  const monthlyPortions = useSelector((state) => state.water.monthlyPortions);
 
   useEffect(() => {
-    
-    dispatch(fetchMonthlyPortionsThunk(new Date())); 
-  }, [dispatch]);
-
+    dispatch(fetchMonthlyPortionsThunk(new Date()));
+  }, [dispatch, dailyNorm]); 
 
   useEffect(() => {
-    // Fetch the monthly portions data whenever the selected month changes
     dispatch(
       fetchMonthlyPortionsThunk({ month: selectedMonth, year: currentYear })
     );
   }, [dispatch, selectedMonth, currentYear]);
 
   useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
+    const handleResize = () => setIsMobile(window.innerWidth <= 768);
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const getDaysInMonth = (month, year) => {
-    return new Date(year, month + 1, 0).getDate();
-  };
+  const getDaysInMonth = (month, year) => new Date(year, month + 1, 0).getDate();
 
   const stats = useMemo(() => {
     const daysInMonth = getDaysInMonth(selectedMonth, currentYear);
-
-    const monthStats = Array.from({ length: daysInMonth }, (_, day) => {
-      const dayData = monthlyPortions?.find((portion) => new Date(portion._id).getDate() === day + 1) || {
-        totalVolume: 0, 
-        servings: 0, 
-        percent: 0,
+    return Array.from({ length: daysInMonth }, (_, day) => {
+      const dayData = monthlyPortions?.find((portion) => 
+        new Date(portion._id).getDate() === day + 1) || {
+          totalVolume: 0, servings: 0, percent: 0,
       };
-
       return {
         day: day + 1,
         waterPercentage: dayData.percent,
-        dailyNorma: dayData.totalVolume, 
+        dailyNorma: dayData.totalVolume,
         servings: dayData.servings,
       };
     });
-
-    return monthStats;
-  }, [selectedMonth, currentYear, monthlyPortions]); 
+  }, [selectedMonth, currentYear, monthlyPortions]);
 
   const handleMonthChange = direction => {
     if (direction === 'prev' && selectedMonth > 0) {
@@ -92,7 +66,6 @@ const MonthStatsTable = () => {
 
   return (
     <div className={styles.monthStatsTable}>
-      {/* Paginator for month selection */}
       <div className={styles.container}>
         <h2 className={styles.title}>Month</h2>
         <div className={styles.paginator}>
@@ -103,9 +76,7 @@ const MonthStatsTable = () => {
           >
             <use href={`${sprite}#icon-right`}></use>
           </svg>
-          <span>
-            {months[selectedMonth]} {currentYear}
-          </span>
+          <span>{months[selectedMonth]} {currentYear}</span>
           {selectedMonth < currentDate.getMonth() && (
             <button onClick={() => handleMonthChange('next')}>
               <svg className={styles.svg}>
@@ -115,26 +86,20 @@ const MonthStatsTable = () => {
           )}
         </div>
       </div>
-
-      {/* List of days with water intake stats */}
       <div className={styles.daysList}>
         {stats.map((dayStat) => (
-          <div className={styles.dayBlockContainer} key={dayStat.day} onClick={() => setSelectedDay(dayStat)}>
-            <div
-              className={`${styles.dayBlock} ${
-                dayStat.waterPercentage < 100 ? styles.incomplete : ''
-              }`}
-            >
+          <div 
+            className={styles.dayBlockContainer} 
+            key={dayStat.day} 
+            onClick={() => setSelectedDay(dayStat)}
+          >
+            <div className={`${styles.dayBlock} ${dayStat.waterPercentage < 100 ? styles.incomplete : ''}`}>
               <span className={styles.dayNumber}>{dayStat.day}</span>
             </div>
-            <span className={styles.dayPercentage}>
-              {dayStat.waterPercentage}%
-            </span>
+            <span className={styles.dayPercentage}>{dayStat.waterPercentage}%</span>
           </div>
         ))}
       </div>
-
-      {/* Detailed stats for selected day */}
       {selectedDay && (
         <DaysGeneralStats
           chosenDay={`${selectedDay.day}.${selectedMonth + 1}.${currentYear}`}
