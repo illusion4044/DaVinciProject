28b2474ab3eeab7a10d1fca38d490eb6875fbef8
illusion4044 <117983189+illusion4044@users.noreply.github.com>
2024-10-31 10:18:37 +0000

    Deploying to gh-pages from @ illusion4044/DaVinciProject@ec95f40ac25c4a9f91c71722b07fb871bcf74f2a 🚀

---
+++ assets/SignupPage-DjK38LwG.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-C_ZNR-43.js";import{A as n}from"./AuthForm-BHokQ9xV.js";const i="_signinpage_1fcbb_7",a={signinpage:i},g=()=>s.jsx("div",{className:a.signinpage,children:s.jsx(n,{})});export{g as default};
